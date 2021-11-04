31f13a4cd86ca15b636be6bf01043edd3dc8a8ee
Kingstalux <kingsleyngu25@gmail.com>
2021-11-04 07:29:36 +0100

    Add calculator function component

---
+++ src/components/Calculator.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
+
+export default function Calculator() {
+
+  return (
+    <div className="body">
+      <button className="btn-input" type="button" value="0">{state.display}</button>
+      <button type="button" onClick={handleClick} value="AC">AC</button>
+      <button type="button" onClick={handleClick} value="+/-">+/-</button>
+      <button type="button" onClick={handleClick} value="%">%</button>
+      <button className="color" type="button" onClick={handleClick} value="÷">÷</button>
+      <button type="button" onClick={handleClick} value="7">7</button>
+      <button type="button" onClick={handleClick} value="8">8</button>
+      <button type="button" onClick={handleClick} value="9">9</button>
+      <button className="color" type="button" onClick={handleClick} value="x">×</button>
+      <button type="button" onClick={handleClick} value="4">4</button>
+      <button type="button" onClick={handleClick} value="5">5</button>
+      {' '}
+      <button type="button" onClick={handleClick} value="6">6</button>
+      <button className="color" type="button" onClick={handleClick} value="-">-</button>
+      <button type="button" onClick={handleClick} value="1">1</button>
+      <button type="button" onClick={handleClick} value="2">2</button>
+      <button type="button" onClick={handleClick} value="3">3</button>
+      <button className="color" type="button" onClick={handleClick} value="+">+</button>
+      <button className="btn-0" type="button" onClick={handleClick} value="0">0</button>
+      {' '}
+      <button type="button" onClick={handleClick} value=".">.</button>
+      <button className="color" type="button" onClick={handleClick} value="=">=</button>
+    </div>
+  );
+}
