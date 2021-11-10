ae722f2893e5990cce3e872d7091e7089cfcc62f
Kingstalux <kingsleyngu25@gmail.com>
2021-11-10 06:18:00 +0100

    Add home.js, quotes.js files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './pages/Home';
+import Quotes from './pages/Quotes';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Calculator />
+      <div>
+        <nav>
+          <div>
+            <p>Maths Magicians</p>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/calculator">Calculator</Link>
+              </li>
+              <li>
+                <Link to="/quote">Quote</Link>
+              </li>
+            </ul>
+          </div>
+
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<div className="app"><Calculator /></div>} />
+          <Route path="/quote" element={<Quotes />} />
+        </Routes>
       </div>
     );
   }
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to our page</h1>
+    </div>
+  );
+}
